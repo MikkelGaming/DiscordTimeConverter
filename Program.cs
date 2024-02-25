eb040a226a0f40da48a432a933bff4791d7c8e0e
MikkelGaming <70634606+MikkelGaming@users.noreply.github.com>
2024-02-25 15:30:46 +0100

    Menu Added
    Menu added for ease of use.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,42 @@
-﻿long UnixTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-Console.WriteLine($"<t:{UnixTime}:F>");
-Console.WriteLine("Input new date\n---Menu---\n1 min = 60\n1 hour = 3600\n24 hours = 86400\n1 week = 604800\n1 month = 2419200\n6 months = 14515200\n 1 year = 29030400\n");
+﻿void Menu()
+{
+    Console.WriteLine("---Menu---\n[1] Current date and time\n[2] Add time onto current date\n[3] Write own date");
+    string? choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            CurrentTime();
+        break;
 
-string addition = Console.ReadLine();
-long.TryParse(addition, out long add);
-UnixTime += add;
-Console.WriteLine($"<t:{UnixTime}:F>");
+        case "2":
+            AdditionTime();
+        break;
+
+        default: 
+            Menu();
+        break;
+
+    }
+} 
+
+void CurrentTime()
+{
+    long UnixTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    Console.WriteLine($"<t:{UnixTime}:F>");
+}
+
+void AdditionTime()
+{
+    long UnixTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    Console.WriteLine("Input new date\n---Menu---\n1 min = 60\n1 hour = 3600\n24 hours = 86400\n1 week = 604800\n1 month = 2419200\n6 months = 14515200\n 1 year = 29030400\n");
+
+    string? addition = Console.ReadLine();
+    long.TryParse(addition, out long add);
+
+    UnixTime += add;
+    Console.WriteLine($"<t:{UnixTime}:F>");
+}
+
+
+Menu();
+
